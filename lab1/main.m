3ad83e24b452971672699c82ecaca01767cd5ea0
Niclas <nbergli@kth.se>
2012-09-18 12:08:23 +0200

    Some cleanup

--- lab1/main.m
+++ lab1/main.m
@@ -9,6 +9,7 @@ L_1 = .4;
 L_2 = .4;
 d_1 = .2;
 
+% Some random test values while testing in phase one (developing functions)
 theta_1 = pi/5;
 theta_2 = pi/5;
 L_3 = .1;
@@ -21,15 +22,7 @@ X_cart = K_f(theta_1, theta_2, L_3);
 % calc joint space coordinates
 Theta = K_i(X_cart(1), X_cart(2), X_cart(3));
 
-% 
-% if size(Theta,1) > 3
-%     X_cart2 = K_f(Theta(4), Theta(5), Theta(6))
-% 
-%     norm(X_cart2 - X_cart) < 0.0001
-%     
-% end
-
-% compare resut to original to see if K_f and K_i works
+% compare result to original to see if K_f and K_i works
 abs(theta_1 - Theta(1)) < 0.0001;
 abs(theta_2 - Theta(2)) < 0.0001;
 abs(L_3 - Theta(3))     < 0.0001;
@@ -50,14 +43,22 @@ end_point   = [-0.5 0.1 0];
 Theta_start = K_i(start_point(1), start_point(2), start_point(3));
 Theta_end   = K_i(end_point(1), end_point(2), end_point(3));
 
-v = (end_point - start_point) / 50;
+v = (end_point - start_point) / 50; % 50 -> just a simple first test value for speed
 
 % ---- move to start pos ---- 
-Theta_i = Theta_start;
-d1      = 0;
-margin                  = 15; % tune this to change trajectory from init to start.
+Theta_i                 = Theta_start;
+d1                      = 0;
+margin                  = 14; % tune this to change trajectory from init to start.
 stepsToLowerManipulator = 21;
 
+% Move the arm to correct start position of weldidng.
+% The margin value where found by trial and error but the number of 
+% frames used for lower the welding torch (21 frames) where calculated
+% on paper (using know time to lower torch (speed of frame and length of
+% torch))
+% We know this is not optimal. We could calculate the optimal
+% speed(in number of frames) by first getting the start coordinate from the simulation but we where to
+% lazy to do that...
 for j=1:stepsToLowerManipulator + margin
     
     trajectory(1,j) = 180/pi * Theta_i(1);
@@ -69,19 +70,24 @@ for j=1:stepsToLowerManipulator + margin
         d1 = d1 + 0.01;
     end
 end
+% Now we are in the start position
 
 % ---- calc optimal speed ---- 
-v_norm = v / norm(v);
-v_min  = inf;
-Theta_old = Theta_start;
+
+v_norm = v / norm(v);   % direction of the speed
+v_min  = inf;           % start value that we want to improve on
+%Theta_old = Theta_start;
 
 % calculate a speed limit on the path by calculating a trajectory with a
 % low velocity first
 while(norm(end_point - K_f(Theta_i(1), Theta_i(2), Theta_i(3))') > 0.01)
     J_inv = inv(jacobian(Theta_i));
+    % Get the next position of arms
     Theta_i = Theta_i + J_inv * v' .* 0.1;
     speed = J_inv * v_norm';
     
+    % Now we determine this change doesn't break the overall max
+    % speed
     next = min((pi/180)*50/abs(speed(1)), (pi/180)*50/abs(speed(2)));
     v_min = min(next, v_min);
 end
@@ -127,7 +133,7 @@ while(norm(end_point - K_f(Theta_i(1), Theta_i(2), Theta_i(3))') > 0.005)
     v_normal         = cross(v_orig / norm(v_orig), [0 0 1]);
     error            = currentPosOnLine * v_normal';
     v                = v_orig - 10 * error * v_normal;
-    v                = v / norm(v) * v_min;
+   % v                = v / norm(v) * v_min;
 end
 
 % ---- Simulate trajectory ----
@@ -139,42 +145,45 @@ X_cart = rob_sim(trajectory',2);
 
 % ---- Plot trajectory (x,y,z & theta_1,theta_2,L_3) ----
 figure()
-time = 0:0.1:length(X_cart)*0.1-0.1;
+
+test = 0
+X_cart = X_cart(2:end,:);
+sim_time = 0:0.1:size(X_cart,1)*0.1-0.1;
 
 % plot x, y, z
 subplot(6,2,1)
 hold on
 xlabel('Time (s)')
 ylabel('X-coordinate (m)')
-plot(time, X_cart(:,1)')
+plot(sim_time, X_cart(:,1)')
 
 subplot(6,2,3)
 hold on
 xlabel('Time (s)')
 ylabel('Y-coordinate (m)')
-plot(time, X_cart(:,2)')
+plot(sim_time, X_cart(:,2)')
 
 subplot(6,2,5)
 hold on
 xlabel('Time (s)')
 ylabel('Z-coordinate (m)')
-plot(time, X_cart(:,3)')
+plot(sim_time, X_cart(:,3)')
 
 % plot theta_1, theta_2, L_3
 subplot(6,2,2)
 hold on
 xlabel('Time (s)')
 ylabel('Theta_1-coordinate (degrees)')
-plot(time(1:length(trajectory)), trajectory(1,:))
+plot(sim_time(1:size(trajectory,2)), trajectory(1,:))
 
 subplot(6,2,4)
 hold on
 xlabel('Time (s)')
 ylabel('Theta_2-coordinate (degrees)')
-plot(time(1:length(trajectory)), trajectory(2,:))
+plot(sim_time(1:size(trajectory, 2)), trajectory(2,:))
 
 subplot(6,2,6)
 hold on
 xlabel('Time (s)')
 ylabel('L3-coordinate (m)')
-plot(time(1:length(trajectory)), trajectory(3,:))
+plot(sim_time(1:size(trajectory,2)), trajectory(3,:))
