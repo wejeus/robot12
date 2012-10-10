b9eceb449c89e58a7a658148645d7c1c967134cf
Hanif FM <hanif@brightlabs.se>
2012-10-10 18:25:20 +0200

    Changed the key_handler to fit the Encoder.msg.

--- amee/src/key_handler.cpp
+++ amee/src/key_handler.cpp
@@ -1,6 +1,7 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 #include "amee/KeyboardCommand.h"
+#include "amee/Encoder.h"
 #include <stdio.h>
 #include <termios.h>
 #include <unistd.h>
@@ -11,10 +12,12 @@
 enum {KEYCODE_SPACE = 32, KEYCODE_U = 65, KEYCODE_D, KEYCODE_R, KEYCODE_L};
 
 /* Holds the velocities of the (left & right) wheels */
-amee::KeyboardCommand v;
+//amee::KeyboardCommand v;
+amee::Encoder v;
 
 /* The volocity change rate */
-float VELO_RATE = 0.5f;
+long VELO_RATE = 1l;
+struct timeval start;
 
 
 
@@ -50,8 +53,12 @@ int kbhit(void){
 /**
  * Sets the wheel velocities to given values
  **/
-inline void setWheels(const float L=0.0f, const float A=0.0f){
-	v.linear = L; v.angular = A;
+inline void setWheels(const long L=0, const long R=0){
+	//v.linear = L; v.angular = A;
+	gettimeofday(&start, NULL);
+	v.left 	= L;
+	v.right = R;
+	v.timestamp = start.tv_sec+double(start.tv_usec)/1000000.0;
 }
 
 
@@ -64,7 +71,7 @@ void startKeyboardHandling(int argc, char **argv){
 	ros::init(argc, argv, "KeyHandler");
 	ros::NodeHandle n;
 
-	ros::Publisher keyCom_pub = n.advertise<amee::KeyboardCommand>("/KeyboardControl/KeyboardCommand", 1000);
+	ros::Publisher keyCom_pub = n.advertise<amee::Encoder>("/serial/encoder", 100000);
 
 	ros::Rate loop_rate(10);
 
@@ -81,19 +88,19 @@ void startKeyboardHandling(int argc, char **argv){
 				switch(c){
 					case KEYCODE_L:
 						ROS_DEBUG("LEFT");
-						setWheels(0, 1);
+						setWheels(0, VELO_RATE);
 						break;
 					case KEYCODE_R:
 						ROS_DEBUG("RIGHT");
-						setWheels(0, -1);
+						setWheels(0, -VELO_RATE);
 						break;
 					case KEYCODE_U:
 						ROS_DEBUG("UP");
-						setWheels(1, 0);
+						setWheels(VELO_RATE, 0);
 						break;
 					case KEYCODE_D:
 						ROS_DEBUG("DOWN");						
-						setWheels(-1, 0);
+						setWheels(-VELO_RATE, 0);
 						break;
 					default:
 						ROS_DEBUG("RELEASE");
@@ -118,72 +125,6 @@ void startKeyboardHandling(int argc, char **argv){
 
 }
 
-/* This is for handling the input from the terminal */
-void terminalHandler(int argc, char **argv){
-	bool inOK = false;
-	int in;
-
-	while(!inOK){
-		printf("\033[2J\033[1;1H");
-		puts("###################################################################\n#");
-		puts("#  1. Navigate the robot with the keyboard arrows.");
-		puts("#  2. Give the arguments (A,D,x,y) for the robot movement.");
-		puts("#  3. Do a random movement.");
-		puts("#");
-		puts("#  0. Quit\n#");
-		puts("###################################################################\n\n");
-	
-		in = int(getchar()) - 48;
-
-		/* if the input values are in the intervall */
-		if(in >= 0 && in <= 3)
-			inOK = true;
-	}
-
-
-	if(in == 0) exit(0);
-
-	// Navigate with keyboard arrows
-	if(in == 1)
-		startKeyboardHandling(argc, argv);
-
-	// Start the MotorControl with some given values
-	if(in == 2){
-		float A, D, x, y;
-		const float NAN_F = -888888.8f;
-		A = D = x = y = NAN_F;
-
-		inOK = false;
-		while(!inOK){
-			printf("Input values for A D x y: ");
-			scanf("%f %f %f %f", &A, &D, &x, &y);
-
-			if(A == NAN_F || D == NAN_F || x == NAN_F || y == NAN_F){//Bad code!
-				printf("\033[2J\033[1;1H"); //clear the screen
-				puts("Did you forget to input all the values? (A D x y)");
-				puts("Try again");
-			}else{
-				inOK = true;
-			}
-		}
-		
-		puts("I need some code to start the FakeMotor and the MotorControl with the given values.");
-		puts("Exiting");
-		exit(1);
-		//TODO: Start the FakeMotor
-		//TODO: Start the Motorcontrol with the values
-		
-	}	
-
-	// Start the MotorControl with some random values
-	if(in == 3){
-		//TODO: code me
-		puts("Not coded yet! Exiting.");
-		exit(0);
-	}
-	
-}
-
 
 int main(int argc, char **argv){
 	
@@ -192,7 +133,6 @@ int main(int argc, char **argv){
 	}
 
 	printf("VELO_RATE is set to: %0.2f\n", VELO_RATE);
-	//terminalHandler(argc, argv);// get input from terminal
 	startKeyboardHandling(argc, argv);
 
 	return 0;
--- amee/src/key_handler_listener.cpp
+++ amee/src/key_handler_listener.cpp
@@ -1,10 +1,11 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 #include "amee/KeyboardCommand.h"
+#include "amee/Encoder.h"
 
 void keyHandlerCallback(const amee::KeyboardCommand & v)
 {
-  ROS_INFO("[%0.2f, %0.2f]", v.linear, v.angular);
+  ROS_INFO("%f:[%i, %i]", v.timestamp, v.left, v.right);
 }
 
 int main(int argc, char **argv)
@@ -13,7 +14,7 @@ int main(int argc, char **argv)
 
   ros::NodeHandle n;
 
-  ros::Subscriber sub = n.subscribe("/KeyboardControl/KeyboardCommand", 1000, keyHandlerCallback);
+  ros::Subscriber sub = n.subscribe("/serial/Encoder", 1000, keyHandlerCallback);
 
   ros::spin();
 
