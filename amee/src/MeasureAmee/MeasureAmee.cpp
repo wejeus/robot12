3b44ce62f0651af94cbb32f160ea0056bd810f20
Robot <robot@desk0.(none)>
2012-11-02 20:50:59 +0100

    Fixed some bugs with the MeasureAmee.

--- amee/src/MeasureAmee/MeasureAmee.cpp
+++ amee/src/MeasureAmee/MeasureAmee.cpp
@@ -8,49 +8,69 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <math.h>
+#include <ctime>
 
 
 using namespace amee;
-using namespace roboard_drivers;
 
+time_t start_t, end_t; /* We need these to let the robot run for a while before stopping her */
+bool first = true;
 float x_first, x_last, y_first, y_last;
-float DIST_ERROR = 0.5; //in meter
+float curDist = 0.0f;
+float DIST_ERROR = 0.2f; //in meter
+float MIN_RUNNING_TIME = 10.0f; //in seconds
 
 bool distReached(){
-	if(sqrt(abs(x_first - x_last) + abs(y_first - y_last)) < DIST_ERROR)
+	if((curDist = sqrt(fabs(x_first - x_last) + fabs(y_first - y_last))) < DIST_ERROR)
 		return true;
 	return false;
 }
 
 void calcPos(const Odometry::ConstPtr &odo){
-	ROS_INFO("x: %0.2f, y: %0.2f", odo->x, odo->y);
+	ROS_INFO("x: %0.2f, y: %0.2f, euclideanDist: %0.2f", odo->x, odo->y, curDist);
+	if(first){
+		x_first = odo->x; y_first = odo->y;
+		first = false;
+		ROS_INFO("#### First x: %0.2f, y: %0.2f ####", x_first, y_first);
+	}
 	x_last = odo->x; y_last = odo->y;
 }
 
 
 int main(int argc, char ** argv){
+	if(argc > 1){
+		DIST_ERROR = atof(argv[1]);
+	}
+	printf("DIST_ERROR is set to %0.2f\n", DIST_ERROR);
+
+
+	/* Init ROS stuff */
 	ros::init(argc, argv, "MeasureAmee");
 	ros::NodeHandle nh;
-	ros::Publisher pub = nh.advertise<MovemenCommand>("/MovementControl/MovementCommand", 1);
+	ros::Publisher pub = nh.advertise<MovementCommand>("/MovementControl/MovementCommand", 1);
 	ros::Subscriber sub = nh.subscribe("/amee/motor_control/odometry", 100, &calcPos);
-	ros::Rate loop_rate(20);
+	ros::Rate loop_rate(10);
 
+	/* Init the start and stop msg to publish */
 	MovementCommand mc, mc_stop;
 	mc.type = 4; mc.distance = mc.x = mc.y = mc.angle = 0.0f;
 	mc_stop.type = 5;
 	x_first = y_first = 0.0f;
 
+	/* wait for subscriber */
+	while(ros::ok() && pub.getNumSubscribers() == 0) loop_rate.sleep();
 
-	while(ros::ok() && pub.getNumSubscribers() == 0) loop_rate.sleep(); /* wait for subscriber */
-
-	pub.publish(mc); /* publish follow wall to MovementControl */
+	/* publish follow wall to MovementControl */
 	ROS_INFO("Publishing start wallfollowing");
-
+	pub.publish(mc);
+	
+	time(&start_t);
 
 	while(ros::ok()){
 		ros::spinOnce();
 		
-		if(distReached){
+		time(&end_t);
+		if(difftime(end_t, start_t) > MIN_RUNNING_TIME && distReached()){
 			pub.publish(mc_stop); /* publishing stop wallfollowing to MovementControl */
 			ROS_INFO("Publishing stop wallfollowing");
 			ros::spinOnce();
