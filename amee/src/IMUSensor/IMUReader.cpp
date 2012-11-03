2be776c70b92f6519448f0dbb175f1822d986725
Hanif FM <hanif@brightlabs.se>
2012-11-03 21:40:37 +0100

    Added IMUReader node.

---
+++ amee/src/IMUSensor/IMUReader.cpp
@@ -0,0 +1,31 @@
+#include "ros/ros.h"
+#include <std_msgs/Int32.h>
+#include "roboard_drivers/imu.h"
+#include <string.h>
+
+using namespace roboard_drivers;
+ros::Subscriber	imu_sub;
+ros::Publisher	int_pub;
+
+void recive_imu(const imu::ConstPtr &msg)
+{
+	//printf("got encoder L:%i , R:%i\n",msg->left,msg->right);
+	printf("acc[%f,%f,%f] gyro[%f,%f,%f] magnetic[%f,%f,%f]\n",msg->acc_x,msg->acc_y,msg->acc_z,msg->gyro_x,msg->gyro_y,msg->gyro_z,msg->comp_x,msg->comp_y,msg->comp_z);
+}
+
+int main(int argc, char **argv)
+{
+	ros::init(argc, argv, "imuNodeTest");
+	ros::NodeHandle n;
+	imu_sub = n.subscribe("/imu", 1000, recive_imu);
+//	int_pub = n.advertise<std_msgs::Int32>("/imu_interval", 100000);
+
+	ros::Rate loop_rate(100);
+	struct timeval start, end;
+
+	while(ros::ok()){
+		loop_rate.sleep();
+		ros::spinOnce();
+	}
+	return 0;
+}
