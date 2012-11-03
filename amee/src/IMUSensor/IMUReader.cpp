c13007c3b56a72d82d66da0ba7a257b02bf27a21
Hanif FM <hanif@brightlabs.se>
2012-11-03 21:45:36 +0100

    Added publisher to the IMUReader.

--- amee/src/IMUSensor/IMUReader.cpp
+++ amee/src/IMUSensor/IMUReader.cpp
@@ -6,11 +6,13 @@
 using namespace roboard_drivers;
 ros::Subscriber	imu_sub;
 ros::Publisher	int_pub;
+ros::Publisher f_imu_pub;
 
 void recive_imu(const imu::ConstPtr &msg)
 {
 	//printf("got encoder L:%i , R:%i\n",msg->left,msg->right);
 	printf("acc[%f,%f,%f] gyro[%f,%f,%f] magnetic[%f,%f,%f]\n",msg->acc_x,msg->acc_y,msg->acc_z,msg->gyro_x,msg->gyro_y,msg->gyro_z,msg->comp_x,msg->comp_y,msg->comp_z);
+	f_imu_pub.publish(msg);
 }
 
 int main(int argc, char **argv)
@@ -18,6 +20,7 @@ int main(int argc, char **argv)
 	ros::init(argc, argv, "imuNodeTest");
 	ros::NodeHandle n;
 	imu_sub = n.subscribe("/imu", 1000, recive_imu);
+	f_imu_pub = n.advertise<imu>("/amee/sensors/imu", 10);
 //	int_pub = n.advertise<std_msgs::Int32>("/imu_interval", 100000);
 
 	ros::Rate loop_rate(100);
