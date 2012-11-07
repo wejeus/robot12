f9ec923b8b5521456fe2b4c7f3b5174f01c1da00
Robot <robot@desk0.(none)>
2012-11-07 14:23:26 +0100

    Fixes and new milestone launch

--- amee/src/Camera/TagDetector.cpp
+++ amee/src/Camera/TagDetector.cpp
@@ -15,11 +15,12 @@
 #include <std_msgs/Int32.h>
 #include <std_msgs/Int8MultiArray.h>
 #include "amee/Tag.h"
+#include <ros/console.h>
+using namespace amee;
 #endif
 
 using namespace std;
 using namespace cv;
-using namespace amee;
 
 void streamCamera(VideoCapture &capture);
 void render(Mat &frame);
@@ -33,25 +34,42 @@ void onCannyHighChange(int value);
 void onCannyLowChange(int value);
 void *onButtonDoBlur(int state, void *pointer);
 void show(const string& winname, InputArray mat);
+void log(char* fmt, ...);
 
 char windowResult[] = "result";
 char windowThresh[] = "tresh";
-int SATURATION_COLOR_HIGH = 139;
-int SATURATION_COLOR_LOW = 83;
+int SATURATION_COLOR_HIGH = 109;
+int SATURATION_COLOR_LOW = 0;
 int CANNY_LOW = 77;
 int CANNY_HIGH = 92;
+double TAG_MIN_PIXEL_AREA = 1500.0;
+
 
 #ifdef ROS
 ros::Publisher mapPublisher;
 #endif
 
 // Flags
-bool SMOOTH_IMAGE = false;
+bool SMOOTH_IMAGE = true;
 bool FILTER_RED_TAG = true;
 bool EQUALIZE_HISTOGRAM = true;
 bool DISPLAY_GRAPHICAL = false;
 bool LOCAL = false;
 
+// Can only handle C types
+void log(char* fmt, ...) {
+    va_list args;
+    va_start(args,fmt);
+
+    #ifdef ROS
+    // ROS_DEBUG(fmt, args);
+    vprintf(fmt, args);
+    #else
+    vprintf(fmt, args);
+    #endif
+
+    va_end(args);
+}
 void show(const string& winname, InputArray mat) {
     if (DISPLAY_GRAPHICAL) {
         imshow(winname.c_str(), mat);
@@ -102,7 +120,7 @@ void filterRedTag(Mat &srcImg, Mat &destImg) {
     Mat binImg;
 
     // void cvtColor(InputArray src, OutputArray dst, int code, int dstCn=0)
-    cvtColor(srcImg, hsvImg, CV_RGB2HSV); // TODO: Change to BGR in ROS?
+    cvtColor(srcImg, hsvImg, CV_BGR2HSV); // TODO: Change to BGR in ROS?
     
     // vector<cv::Mat> hsvChannels;
     // split(hsvImg, hsvChannels);
@@ -110,7 +128,7 @@ void filterRedTag(Mat &srcImg, Mat &destImg) {
     // if (EQUALIZE_HISTOGRAM) equalizeHist(hsvImg, hsvImg);
 
     // void inRange(InputArray src, InputArray lowerb, InputArray upperb, OutputArray dst)
-    inRange(hsvImg, Scalar(SATURATION_COLOR_LOW, 100, 100), Scalar(SATURATION_COLOR_HIGH, 255, 255), binImg);
+    inRange(hsvImg, Scalar(SATURATION_COLOR_LOW, 0, 0), Scalar(SATURATION_COLOR_HIGH, 255, 255), binImg);
     
     // void Canny(InputArray image, OutputArray edges, double threshold1, double threshold2, int apertureSize=3, bool L2gradient=false )
     Canny(binImg, binImg, CANNY_LOW, CANNY_HIGH);
@@ -122,7 +140,8 @@ void filterRedTag(Mat &srcImg, Mat &destImg) {
 
     for(int i = 0; i < contours.size(); ++i) {
         RotatedRect rect = minAreaRect(contours[i]);
-        if (contourArea(contours[i]) > 1000) {
+        if (contourArea(contours[i]) > TAG_MIN_PIXEL_AREA) {
+            log("AREA: %f\n", contourArea(contours[i]));
             Point2f vtx[4];
             rect.points(vtx);
             for(int j = 0; j < 4; ++j) {
@@ -133,15 +152,15 @@ void filterRedTag(Mat &srcImg, Mat &destImg) {
 
             int diff = imageCenter - rect.center.x;
 
-            if (abs(diff) < 20) {
-                cout << diff << endl;
+            if (abs(diff) < 40) {
+                log("TAG FOUND! Distance from normal: %d\n", diff);
                 circle(destImg, rect.center, 20, Scalar(0, 0, 255), 10);
                 #ifdef ROS
                 // mark tag on map!
-                Tag tag;
-                tag.distance = diff;
-                tag.side = 1;
-                mapPublisher.publish(tag);
+                // Tag tag;
+                // tag.distance = diff;
+                // tag.side = 1;
+                // mapPublisher.publish(tag);
                 #endif
             }
             
@@ -154,34 +173,34 @@ void filterRedTag(Mat &srcImg, Mat &destImg) {
 
 
 void onSaturationColorHighChange(int value) {
-    cout << "SATURATION_COLOR_HIGH: " << value << endl;
+    log("SATURATION_COLOR_HIGH: %d\n", value);
     SATURATION_COLOR_HIGH = value;
 }
 
 void onSaturationColorLowChange(int value) {
-    cout << "SATURATION_COLOR_LOW: " << value << endl;
+    log("SATURATION_COLOR_LOW: %d\n", value);
     SATURATION_COLOR_LOW = value;
 }
 
 void onCannyHighChange(int value) {
-    cout << "CANNY_HIGH: " << value << endl;
+    log("CANNY_HIGH: %d\n", value);
     CANNY_HIGH = value;
 }
 
 void onCannyLowChange(int value) {
-    cout << "CANNY_LOW: " << value << endl;
+    log("CANNY_LOW: %s\n", value);
     CANNY_LOW = value;
 }
 
 void *onButtonDoBlur(int state, void *pointer) {
-    printf("ok");
+    log("ok\n");
 }
 
 #ifdef ROS
 void cam0_cb(const std_msgs::Int8MultiArray::ConstPtr& array) {
     IplImage *img               = cvCreateImage(cvSize(320, 240), IPL_DEPTH_8U, 3);
     char * data                 = img->imageData;
-    cout << "adsf" << endl;
+
     for(int i = 0; i < 320*240*3; i++)
     {
         data[i] = char(array->data.at(i));
@@ -198,7 +217,7 @@ void cam0_cb(const std_msgs::Int8MultiArray::ConstPtr& array) {
 
 void initROS(int argc, char *argv[]) {
     #ifdef ROS
-    printf("Starting TagDetection using ROS\n");
+    log("Starting TagDetection using ROS\n");
     ros::init(argc, argv, "TagDetection");
     ros::NodeHandle n;
 
@@ -208,26 +227,26 @@ void initROS(int argc, char *argv[]) {
     // ros::Subscriber img1_sub = n.subscribe("/camera1_img", 1, cam1_cb);
     ros::spin();
     #else
-    printf("ROS IS NOT DEFINED");
+    log("ROS IS NOT DEFINED");
     #endif
 }
 
 void initLocalInput(string source) {
 
-    cout << "Starting TagDetection using local input" << endl;
+    log("Starting TagDetection using local input.\n");
 
     if (!strcmp(source.c_str(), "")) {
-        cout << "No input source!" << endl;
+        log("No input source!\n");
         return;
     }
 
     VideoCapture capture;
 
     if (isdigit(*source.c_str())) {
-        cout << "Initializing camera: " << source << endl;
+        log("Initializing camera: %s\n", source.c_str());
         capture.open(atoi(source.c_str()));
         if (!capture.isOpened()) {
-            fprintf(stderr, "ERROR: capture is NULL\n");
+            log("ERROR: capture is NULL\n");
             return;
         }
     }
@@ -236,7 +255,7 @@ void initLocalInput(string source) {
         streamCamera(capture);
         // cvReleaseCapture(&capture);
     } else {
-        cout << "Reading image: " << source << endl;
+        log("Reading image: %s\n", source.c_str());
         Mat image = imread(source.c_str(), 1);
         render(image);
         waitKey();
@@ -260,11 +279,11 @@ int main(int argc, char *argv[]) {
                 break;
             case '?':
                 if (optopt == 's')
-                    fprintf (stderr, "Option -%c requires an argument. Image or camera.\n", optopt);
+                    log("Option -%c requires an argument. Image or camera.\n", optopt);
                 else if (isprint (optopt))
-                    fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+                    log("Unknown option `-%c'.\n", optopt);
                 else
-                    fprintf (stderr,"Unknown option character `\\x%x'.\n", optopt);
+                    log("Unknown option character `\\x%x'.\n", optopt);
                 return 1;
             default:
                 abort();
@@ -272,7 +291,7 @@ int main(int argc, char *argv[]) {
     }
 
     if (DISPLAY_GRAPHICAL) {
-        cout << "Will do graphical display" << endl;
+        log("Will do graphical display\n");
         initWindows();
     }
 
@@ -282,7 +301,7 @@ int main(int argc, char *argv[]) {
         initROS(argc, argv);
     }
 
-    cout << "Quitting ..." << endl;
+    log("Quitting ...");
     
     if (DISPLAY_GRAPHICAL) cvDestroyWindow(windowResult);
     return 0;
