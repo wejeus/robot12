e8cad11ce4b8711a88ba4b4c7881ab4d3c84de5d
Robot <robot@robot-vm.(none)>
2012-11-07 12:53:45 +0100

    Localize node that compiles, untested

--- amee/src/Localize/Localize.cpp
+++ amee/src/Localize/Localize.cpp
@@ -5,50 +5,39 @@
 #include <cmath>
 #include "../Common/RobotConstants.h"
 #include "Localize.h"
+#include "EKF.h"
+//#include "roboard_drivers/Encoder"
 
 void Localize::init() 
 {
-	//mMeasurementValidCounter = 2 * NUM_AVERAGED_MEASUREMENTS;
-	//mMeasurementCounter = 0;
 
-	ros::Subscriber	enc_sub;
-	ros::Subscriber	motorSpeed_sub;
-
-	ros::Publisher pose_pub
-
-	ros::Rate loop_rate(40);
 
 }
 
-void Localize::receiveSpeed(const amee::Velocity::ConstPtr &v) {
+void Localize::receiveMotorSpeed(const amee::Velocity::ConstPtr &v) {
 	speed.left  = v->left;
 	speed.right = v->right;
 	speed.linear = (v->left + v->right)/2;
 	speed.angular = (v->left - v->right)/WHEEL_BASE;
 }
 
-void Localize::receiveEncoder(const amee::Odometry::ConstPtr &odo) {
-	z.left = odo->left;
-	z.right = odo->right;
+void Localize::receiveEncoder(const amee::Odometry::ConstPtr &msg) {
+	encoderMeasurement.leftWheelDistance = msg->leftWheelDistance;
+	encoderMeasurement.rightWheelDistance = msg->rightWheelDistance;
 }
 
-void Localize::receiveControlSignal(const amee::Velocity::ConstPtr &controlV) {
-	u.left  = controlV->left;
-	u.right = controlV->right;
-	u.linear = (controlV->left + controlV->right)/2;
-	u.angular = (controlV->left - controlV->right)/WHEEL_BASE;
+void Localize::receiveControlSignal(const amee::Velocity::ConstPtr &ctrl) {
+	controlSignal.left  = ctrl->left;
+	controlSignal.right = ctrl->right;
+	controlSignal.linear = (ctrl->left + ctrl->right)/2;
+	controlSignal.angular = (ctrl->left - ctrl->right)/WHEEL_BASE;
 }
 
-void Localize::publishPose() {
+void Localize::publishPose(ros::Publisher pose_pub) {
 	pose_pub.publish(pose);
 }
 
 // --- EKF functions ---
-void EKF::init(){
-
-
-}
-
 void EKF::setQ(float processNoise){
 	Q(0,0) = processNoise;
 	Q(1,1) = processNoise;
@@ -59,16 +48,20 @@ void EKF::setR(float measurementNoise){
 	R(1,1) = measurementNoise;
 }
 
-Vector3f EKF::g(Vector2f u, Vector3f mu_t_1) { // velocity motion model
-	// move to .h?
-	Vector3f mu_bar;
-	Vector3f movement;
+amee::Pose EKF::getPose() {
+	return pose;
+}
 
-	float dt    = ; // from timestamps 
+Eigen::Vector3f EKF::g(Eigen::Vector2f u, Eigen::Vector3f mu_t_1) { // velocity motion model
+	// move to .h?
+	//Eigen::Vector3f mu_bar;
+	Eigen::Vector3f movement;
+
+	float dt    = 1; // TODO: take from timestamps 
 	float v     = (u(0) + u(1)) /2;
 	float omega = (u(0) - u(1)) / WHEEL_BASE;
 
-	// from probabilistic  robotics p. 127
+	// Velocity motion model. From probabilistic  robotics p. 127
 	movement(0) = -v/omega * sin(mu_t_1(2) + v/omega * sin(mu_t_1(2) + omega*dt));
 	movement(1) = +v/omega * cos(mu_t_1(2) - v/omega * cos(mu_t_1(2) + omega*dt));
 	movement(2) = mu_t_1(2) + omega*dt;
@@ -77,62 +70,65 @@ Vector3f EKF::g(Vector2f u, Vector3f mu_t_1) { // velocity motion model
 	return mu_bar;
 }
 
-Vector2f EKF::h(Vector3f mu_bar, Vector3f mu_bar_t_1) { // odometry motion model
-	Vector3f delta_mu_bar = mu_bar - mu_bar_t_1;
-	Vector2f delta_z_hat;
+Eigen::Vector3f EKF::h(Eigen::Vector3f mu_bar, Eigen::Vector3f mu_bar_t_1) { // odometry motion model
+	
+	// Define relative movement
+	Eigen::Vector3f delta_mu_bar = mu_bar - mu_bar_t_1;
+	Eigen::Vector3f z_hat;
 
-	delta_z_hat(0) = sqrt(pow(delta_mu_bar(0), 2) + pow(delta_mu_bar(1), 2)) + delta_mu_bar(2) * WHEEL_BASE/2;
-	delta_z_hat(1) = sqrt(pow(delta_mu_bar(0), 2) + pow(delta_mu_bar(1), 2)) - delta_mu_bar(2) * WHEEL_BASE/2;
+	// Calc predicted measurement
+	z_hat(0) = sqrt(pow(delta_mu_bar(0), 2) + pow(delta_mu_bar(1), 2)) + delta_mu_bar(2) * WHEEL_BASE/2;
+	z_hat(1) = sqrt(pow(delta_mu_bar(0), 2) + pow(delta_mu_bar(1), 2)) - delta_mu_bar(2) * WHEEL_BASE/2;
+	z_hat(2) = delta_mu_bar(2);
 
-	return delta_z_hat;
+	return z_hat;
 }
-void ekf_main(amee::Pose pose, amee::Pose poseVariance, controlSignal, measurement)
+void EKF::estimate(amee::Pose pose, amee::Velocity controlSignal, amee::Odometry measurement)
 {
-	// Create filter
-	EKF ekf;
-	ekf.init();
-	ekf.serR(1.0f); // set measurement noise
-	ekf.serQ(1.0f); // set process noise
-
 	//Notation taken from Probabilistic robotics
 
-	// --- convert to matrix form ---
+	// --- convert to matrix form --- move to function???
 	// Last pose
-	Vector3f mu_t_1;
+	//Eigen::Vector3f mu_t_1;
 	mu_t_1(0) = pose.x;
 	mu_t_1(1) = pose.y;
 	mu_t_1(2) = pose.theta;
 
-	Matrix3f::Identity sigma_t_1; // matrix from beginning
+	//Eigen::Matrix3f sigma_t_1; 
+	sigma_t_1.Identity();	// matrix from beginning
 
 	// Control signal
-	Vector2f u;
+	//Eigen::Vector2f u;
 	u(0) = controlSignal.left;
-	u(1) = controlSignal.rigt;
+	u(1) = controlSignal.right;
 
 	// Last measurement
-	Vector2f z_t_1;
-	z_t_1(0) = z(0);
-	z_t_1(1) = z(1);
+	//Eigen::Vector3f z_t_1;
+	//z_t_1(0) = z(0);
+	//z_t_1(1) = z(1);
+	//z_t_1(2) = z(2);
 
-	// Measurement
-	Vector2f z;
-	z(0) = measurement.left;
-	z(1) = measurement.right;	
+	// Measurement (movement from last measurement)
+	//Eigen::Vector3f z;
+	z(0) = measurement.leftWheelDistance;
+	z(1) = measurement.rightWheelDistance;	
+	z(2) = (measurement.leftWheelDistance - measurement.rightWheelDistance) / WHEEL_BASE;
 
 	// initiate (move to .h???)
-	Vector3f mu_bar;    // 3by1
-	Matrix3f sigma_bar; // 3by3
-	Vector2f delta_z;   
-	Vector2f delta_z_hat;
-	Matrix3f::Identity I;
+	//Eigen::Vector3f mu_bar;    // 3by1
+	//Eigen::Matrix3f sigma_bar; // 3by3
+	//Eigen::Vector3f delta_z = z - z_t_1;   
+	//Eigen::Vector3f delta_z_hat;
+	//Eigen::Matrix3f I;
+	
+	I.Identity();
 
 	// --- EKF algorithm ---
-	mu_bar      = EFK::g(u, mu_t_1); // TODO need dt in g-function!!!!!!!!1
+	mu_bar      = EKF::g(u, mu_t_1); // TODO need dt in g-function!!!!!!!!1
 	sigma_bar   = G * sigma_t_1 * G.transpose() + R;
-	K           = sigma_bar * H.transpose() * (H * sigma_bar * H.transpose() + Q);
-	delta_z_hat = EKF::h(delta_mu_bar);
-	mu          = mu_bar + K * (delta_z - delta_z_hat);
+	K           = sigma_bar * H.transpose() * (H * sigma_bar * H.transpose() + Q); // TODO K is not a float, Matrix?
+	z_hat       = EKF::h(mu_bar, mu_bar_t_1);
+	mu          = mu_bar + K * (z - z_hat);
 	sigma       = (I - K * H) * sigma_bar;
 
 	// --- Copy data to pose types ---
@@ -140,39 +136,62 @@ void ekf_main(amee::Pose pose, amee::Pose poseVariance, controlSignal, measureme
 	pose.y = mu(1);
 	pose.theta = mu(2);
 
-	poseVariance.x = sigma(0,0);
-	poseVariance.y = sigma(1,1);
-	poseVariance.theta = sigma(2,2);
+	pose.xVar = sigma(0,0);
+	pose.yVar = sigma(1,1);
+	pose.thetaVar = sigma(2,2);
 
 }
 
 int main(int argc, char **argv) 
 {
 	// -- Initiate ROS node
-	ros::init(argc, argv, "Localize");//Creates a node named "Localize"
+	ros::init(argc, argv, "Localize"); //Creates a node named "Localize"
 	ros::NodeHandle n;
 
 	// -- Initialize localizer
-	Localize lozalize;
+	Localize localize;
 	localize.init();
 
 	// -- Subscribe & Publish topics
-	enc_sub        = n.subscribe("/serial/encoder", 100, &Localize::receiveEncoder, &localize);         // Subscribe encoder
-	motorSpeed_sub = n.subscribe("/serial/motor_speed", 100, &Localize::receiveMotorSpeed, &localize);  // Subscribe motorSpeed
-	pose_pub       = n.advertise<Pose>("/pose", 100);                                                   // Publish pose
+	ros::Subscriber enc_sub;
+	ros::Subscriber motorSpeed_sub;
+	ros::Subscriber controlSignal_sub;
+	ros::Publisher  pose_pub;
+
+	enc_sub           = n.subscribe("/serial/encoder", 100, &Localize::receiveEncoder, &localize);              // Subscribe encoder
+	motorSpeed_sub    = n.subscribe("/serial/motor_speed", 100, &Localize::receiveMotorSpeed, &localize);     // Subscribe motorSpeed
+	controlSignal_sub = n.subscribe("/serial/motor_speed", 100, &Localize::receiveControlSignal, &localize); // Subscribe contrlSignal
+
+	pose_pub = n.advertise<amee::Pose>("/pose", 100); // Publish pose
 	
+	ros::Rate loop_rate(40);
+
+	// Create filter
+	EKF ekf;
+	//ekf.init();
+	ekf.setR(1.0f); // set measurement noise
+	ekf.setQ(1.0f); // set process noise
+	ekf.G.Identity();
+	ekf.H.Identity();
 
 	// -- Estimate pose
-	while()
+	while(true)
 	{
-		loop_rate.sleep(); // go to sleep for a short while
+		localize.lastEncoderMeasurement = localize.encoderMeasurement; // Save last before spinOnce
+
 		ros::spinOnce();   // call all callbacks
-		localize.estimatePose(); // estimate pose
+
+		localize.measurement.leftWheelDistance  = localize.encoderMeasurement.leftWheelDistance  - localize.lastEncoderMeasurement.leftWheelDistance;
+		localize.measurement.rightWheelDistance = localize.encoderMeasurement.rightWheelDistance - localize.lastEncoderMeasurement.rightWheelDistance;
+		//localize.measurement.IMUtheta = ;...
 		
+		ekf.estimate(localize.pose, localize.controlSignal, localize.measurement); // have pose in the init function instead!
+		loop_rate.sleep(); // go to sleep for a short while
+
 	}
 
-
 	// -- Publish pose
-	//localize.publishPose();
+	localize.publishPose(pose_pub);
 
+	return 0;
 }
--- amee/src/Localize/Localize.h
+++ amee/src/Localize/Localize.h
@@ -5,84 +5,41 @@
 #include "amee/Velocity.h"
 #include "amee/Odometry.h"
 #include "amee/Pose.h"
-#include <Eigen>
+#include "Eigen/Eigen"
 
-class Loclasize {
+class Localize {
 
 #define X 0.0f
 
 private:
 
+public:
+
 	amee::Velocity speed;
-	amee::Odometry encoder;
-	amee::Odometry lastEncoder;
+
+	amee::Odometry encoderMeasurement;
+	amee::Odometry lastEncoderMeasurement;
+
+	//amee::IMU imuMeasurement;
+
+	amee::Odometry measurement;
+
 	amee::Velocity controlSignal;
-	amee::Pose pose;
+	
+	amee::Pose pose; // set to zero as default
  	amee::Pose lastPose;
+	amee::Pose newPose; 
 
- 	ros::Subscriber enc_sub;
-	ros::Subscriber motorSpeed_sub
-	ros::Publisher pose_pub;
+	void init();
 
-	public:
-		void receiveEncoder(const roboard_drivers::Encoder::ConstPtr &msg);
-		void receiveMotorSpeed(const amee::Velocity::ConstPtr &v);
-		void receiveControlSignal(const amee::Velocity::ConstPtr &v);
-		void publishPose(float x, float y, float theta, float x_var, float y_var, float theta_var);
-		void init();
-
-	protected:
-		// what is protected?
-	private:
-		// why two private?		
-		void publishPose();
-};
-
-class EKF {
-
-	Vector3f mu;
-	Vector3f mu_bar;
-	Vector3f mu_t_1;
-
-	Matrix3f sigma;
-	Matrix3f sigma_bar;
-	Matrix3f sigma_t_1;
+	void receiveEncoder(const amee::Odometry::ConstPtr &msg);
+	void receiveMotorSpeed(const amee::Velocity::ConstPtr &v);
+	void receiveControlSignal(const amee::Velocity::ConstPtr &msg);
 	
-	Vector2f u;
-	Vector2f u_t_1;	
+	void publishPose(ros::Publisher pose_pub);
+
+protected:
 	
-	Vector2f z;
-	Vector2f z_t_1;
-	Vector2f z_hat;
-
-	Matrix3f::Identity G; // jacobian of g (motion model)
-	Matrix3f::Identity H; // Jacobian of h ()
-
-	float K; // Kalman gain
-
-	Matrix2f R; // Measurement noice
-	Matrix2f Q; // Process noice
-
-	amee::Pose pose;
-	amee::Pose poseVariance;
-	
-	private:
-
-		public:
-				void init();
-
-				void g(u, mu_t_1); // calc expected mean
-				void calcExpCov(G, sigma_t_1, R);
-				void calcK(sigma_bar, H, Q);
-				void h(mu_bar);
-				void calcMean(mu_bar, K, H, sigma_bar);
-				void calcCov(K, H, sigma_bar);
-
-				void setR(float processNoise);
-				void setQ(float measurementNoise);
-
-				amee::Pose getMu();
-				amee::Pose getSigma();
-
+		
 };
 #endif
