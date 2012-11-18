e7d4b74febbcdcf52f9ef630297139f569d1d354
Robot <robot@robot-vm.(none)>
2012-11-18 20:08:56 +0100

    switched to odometry localizer

--- amee/src/Localize/Localize.cpp
+++ amee/src/Localize/Localize.cpp
@@ -8,48 +8,50 @@
 #include "roboard_drivers/Encoder.h"
 #include "EKF.h"
 #include "Eigen/Eigen"
+#include "../MovementControl/MoveFollowWall.h"
 //#include "roboard_drivers/Encoder"
+
 
 void Localize::init() 
 {
-	controlSignal.left    = 0;
-	controlSignal.right   = 0;
-	controlSignal.linear  = 0;
-	controlSignal.angular = 0;
+	ros::spinOnce();
+	mPose.x = mOdometry.x;
+	mPose.y = mOdometry.y;
+	mPose.theta = mOdometry.angle;
 
-	encoderMeasurement.left  = 0;
-	encoderMeasurement.right = 0;	
-
-	speed.left    = 0;
-	speed.right   = 0;
-	speed.linear  = 0;
-	speed.angular = 0;
 }
 
-void Localize::receiveMotorSpeed(const roboard_drivers::Motor::ConstPtr &v) {
-	speed.left  = v->left;
-	speed.right = v->right;
-	speed.linear = (v->left + v->right)/2;
-	speed.angular = (v->left - v->right)/WHEEL_BASE;
+void Localize::receiveOdometry(const amee::Odometry::ConstPtr &msg) {
+	mLastOdometry = mOdometry;
+	std::cout << mLastOdometry.timestamp << " " << mOdometry.timestamp << std::endl;
+	mOdometry.x = msg->x;
+	mOdometry.y = msg->y;
+	mOdometry.angle = msg->angle;
+	mOdometry.timestamp = msg->timestamp;
+	mOdometry.leftWheelDistance = msg->leftWheelDistance;
+	mOdometry.rightWheelDistance = msg->rightWheelDistance;
+	//std::cout << "Odometry " << mOdometry.x << " " << mOdometry.y << " " << mOdometry.angle << std::endl;
+	mPose.theta += mOdometry.angle - mLastOdometry.angle;
+
+	// Calc x & y with corrected theta
+	float leftDistance = mOdometry.leftWheelDistance - mLastOdometry.leftWheelDistance;
+	float rightDistance = mOdometry.rightWheelDistance - mLastOdometry.rightWheelDistance;
+	float distance = (leftDistance + rightDistance) / 2.0f;
+
+	//std::cout << "distance: " << leftDistance << " " << rightDistance << std::endl;
+
+	mPose.x += cos(mPose.theta / 180.0f * M_PI) * distance;
+	mPose.y += sin(mPose.theta / 180.0f * M_PI) * distance;
+	mPose.theta = mPose.theta - floor(mPose.theta / 360) * 360.0f;
 }
 
-void Localize::receiveEncoder(const roboard_drivers::Encoder::ConstPtr &msg) {
-	encoderMeasurement.left = msg->left;
-	encoderMeasurement.right = msg->right;
+void Localize::receiveFollowWallState(const amee::FollowWallStates::ConstPtr &msg) {
+	followWallState.state = msg->state;
+	if(followWallState.state == amee::MoveFollowWall::ALIGNED_TO_WALL) {
+		mPose.theta -= mPose.theta - floor(mPose.theta / 90.0f + 0.5f) * 90.0f;
+	}
+	mPose.theta = mPose.theta - floor(mPose.theta / 360) * 360.0f;
 }
-
-void Localize::receiveControlSignal(const amee::Velocity::ConstPtr &ctrl) {
-	controlSignal.left  = ctrl->left;
-	controlSignal.right = ctrl->right;
-	controlSignal.linear = (ctrl->left + ctrl->right)/2;
-	controlSignal.angular = (ctrl->left - ctrl->right)/WHEEL_BASE;
-}
-
-void Localize::publishPose(ros::Publisher pose_pub) {
-	pose_pub.publish(pose);
-}
-
-
 
 int main(int argc, char **argv) 
 {
@@ -62,53 +64,24 @@ int main(int argc, char **argv)
 	localize.init();		
 
 	// -- Subscribe & Publish topics
-	ros::Subscriber enc_sub;
-	ros::Subscriber motorSpeed_sub;
-	ros::Subscriber controlSignal_sub;
+	ros::Subscriber odo_sub;
+	ros::Subscriber followWallState_sub;
 	ros::Publisher  pose_pub;
 
-	enc_sub           = n.subscribe("/serial/encoder",     100, &Localize::receiveEncoder, &localize);       // Subscribe encoder
-	motorSpeed_sub    = n.subscribe("/serial/motor_speed", 100, &Localize::receiveMotorSpeed, &localize);    // Subscribe motorSpeed
-	controlSignal_sub = n.subscribe("/amee/motor_control/set_wheel_velocities", 100, &Localize::receiveControlSignal, &localize); // Subscribe contrlSignal
+	odo_sub            = n.subscribe("/amee/motor_control/odometry", 100, &Localize::receiveOdometry, &localize);  // Subscribe encoder
+	followWallState_sub= n.subscribe("/amee/follow_wall_states", 100, &Localize::receiveFollowWallState, &localize); // Subscribe motorSpeed
 
 	pose_pub = n.advertise<amee::Pose>("/amee/pose", 100); // Publish pose
 	
 	ros::Rate loop_rate(40);
 
-	// Initiate Kalman filter
-	EKF ekf;
-	ekf.init();
-
-	Matrix<float,nos,nos> G;
-	Matrix<float,nom,nos> H;
-
-	G.setIdentity();
-	
-	ekf.setG(G);
-	//ekf.setH(H);
-
-
 	// -- Estimate pose
 	while(ros::ok())
 	{
-		localize.lastEncoderMeasurement = localize.encoderMeasurement; // Save last before updating with spinOnce
-
 		ros::spinOnce(); // call all callbacks
 
-		// Calc relative measurements in tics
-		localize.measurement.left  = (localize.encoderMeasurement.left  - localize.lastEncoderMeasurement.left) * TICS_PER_REVOLUTION / (2.0f * M_PI * WHEEL_RADIUS);;
-		localize.measurement.right = (localize.encoderMeasurement.right - localize.lastEncoderMeasurement.right) * TICS_PER_REVOLUTION / (2.0f * M_PI * WHEEL_RADIUS);;
-		//localize.measurement.IMUtheta = ;...
-		
-		// Estimate pose w kalman filter
-		ekf.estimate(localize.controlSignal, localize.measurement);
-		
-		// Get pose from kalman filter
-		localize.pose = ekf.getPose();
-		
 		// -- Publish pose
-		localize.publishPose(pose_pub);
-
+		pose_pub.publish(localize.mPose);
 		// Sleep for a while
 		loop_rate.sleep(); 
 
--- amee/src/Localize/Localize.h
+++ amee/src/Localize/Localize.h
@@ -4,6 +4,7 @@
 #include <std_msgs/Int32.h>
 #include "amee/Velocity.h"
 #include "amee/Odometry.h"
+#include "amee/FollowWallStates.h"
 #include "amee/Pose.h"
 #include "roboard_drivers/Encoder.h"
 #include "roboard_drivers/Motor.h"
@@ -16,27 +17,15 @@ private:
 
 public:
 
-	amee::Velocity speed;
-
-	roboard_drivers::Encoder encoderMeasurement;
-	roboard_drivers::Encoder lastEncoderMeasurement;
-
-	//amee::IMU imuMeasurement;
-
-	roboard_drivers::Encoder measurement;
-
-	amee::Velocity controlSignal;
-	
-	amee::Pose pose; // set to zero as default
- 	amee::Pose lastPose;
-	amee::Pose newPose; 
+	amee::Odometry mOdometry;
+	amee::Odometry mLastOdometry;
+	amee::Pose mPose; // set to zero as default
+	amee::FollowWallStates followWallState;
 
 	void init();
 
-	void receiveEncoder(const roboard_drivers::Encoder::ConstPtr &msg);
-	void receiveMotorSpeed(const roboard_drivers::Motor::ConstPtr &v);
-	void receiveControlSignal(const amee::Velocity::ConstPtr &msg);
-	
+	void receiveOdometry(const amee::Odometry::ConstPtr &msg);
+	void receiveFollowWallState(const amee::FollowWallStates::ConstPtr &msg);
 	void publishPose(ros::Publisher pose_pub);
 
 protected:
