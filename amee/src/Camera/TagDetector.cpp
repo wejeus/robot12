d23e94a336276c392a13f9e5210bdc0752e5b7f5
Robot <robot@desk0.(none)>
2012-11-19 20:35:26 +0100

    added output of classfied images

--- amee/src/Camera/TagDetector.cpp
+++ amee/src/Camera/TagDetector.cpp
@@ -11,8 +11,10 @@
 #include <string>
 #include <unistd.h>
 #include <sys/time.h>
+#include <sstream>
+#include <string>
 
-// #define ROS
+#define ROS
 
 #ifdef ROS
 #include "ros/ros.h"
@@ -45,7 +47,7 @@ bool EQUALIZE_HISTOGRAM = true;
 bool DISPLAY_GRAPHICAL = false;
 int CAMERA_INTERVAL = 10;
 bool USE_TEMPLATES = false;
-bool INIT_ROS = false;
+bool USE_ROS = false;
 bool CLASSIFICATION_IN_PROGRESS = false;
 bool WAIT_FOR_TIMEOUT = false;
 bool SOURCE_IS_SINGLE_IMAGE = false;
@@ -137,7 +139,7 @@ int main(int argc, char *argv[]) {
                 USE_TEMPLATES = true;     
                 break;
             case 'r':
-                INIT_ROS = true;     
+                USE_ROS = true;     
                 break;
             case 'w':
                 WAIT_FOR_TIMEOUT = true;     
@@ -172,7 +174,7 @@ int main(int argc, char *argv[]) {
         return -1;
     }
 
-    if (INIT_ROS) {
+    if (USE_ROS) {
         initROS(argc, argv);
     }
 
@@ -182,6 +184,17 @@ int main(int argc, char *argv[]) {
     
     if (DISPLAY_GRAPHICAL) cvDestroyWindow(windowResult);
     return 0;
+}
+
+int id = 0;
+char buf[100];
+void imout(Mat &frame, string type) {
+    std::ostringstream ostr;
+    ostr << type << id << ".jpg";
+    std::string s = ostr.str();
+    //imwrite(strcat(strcat(type.c_str(), s.c_str()), ".jpg"), frame);
+    imwrite(s, frame);
+    id++;
 }
 
 // TODO: Goal -> Classification block should not run so often...
@@ -207,6 +220,7 @@ void render(Mat &frame) {
         if (findTagROI(frame, thresholdedImage, ROI)) {
 
             if (!prepareROI(ROI, ROI)) {
+                imout(frame, "noPrepare_");
                 log("Could not prepare ROI!\n");
             }
 
@@ -219,7 +233,10 @@ void render(Mat &frame) {
             log("Object: %s\n", class2name(res).c_str()); 
             setNextTagTimout();
 
+            imout(frame, "success_");
+            
         } else {
+            imout(frame, "noFindRect_");
             log("Could not find (possible) tag rectangle in image\n");
         }
                 
@@ -227,7 +244,8 @@ void render(Mat &frame) {
 
         CLASSIFICATION_IN_PROGRESS = false;           
     } else {
-        log("Could not find enough red pixels in source image\n");
+        imout(frame, "fail_");
+        log("Could not find enough red pixels in source image or timeout blocked\n");
     }
 
     show(windowResult, frame);
@@ -485,7 +503,7 @@ void initROS(int argc, char *argv[]) {
 
     mapPublisher = rosNodeHandle.advertise<Tag>("/amee/tag", 100);
     movementPublisher = rosNodeHandle.advertise<MovementCommand>("/MovementControl/MovementCommand", 1);
-    rawMotorPublisher = rosNodeHandle.advertise<Motor>("/serial/motor_speed", 100);
+//    rawMotorPublisher = rosNodeHandle.advertise<Motor>("/serial/motor_speed", 100);
 
     #else
     log("ROS IS NOT DEFINED");
@@ -510,7 +528,7 @@ bool captureSingleFrame(Mat &frame) {
     // }
     // return true;
 
-    capture->open(CAMERA_ID);
+    //capture->open(CAMERA_ID);
 
     if (CAPTURE_HIGHRES_IMAGE) {
         capture->set(CV_CAP_PROP_FRAME_WIDTH, 640);
@@ -524,6 +542,7 @@ bool captureSingleFrame(Mat &frame) {
 
     return true;
 }
+
 
 // TODO: Stream from video file
 void stream(string source) {
@@ -549,6 +568,8 @@ void stream(string source) {
     } else {
         cout << "Streaming started..." << endl;
 
+capture->open(CAMERA_ID);
+        
         while (true) {
             Mat frame;
             if ( ! CLASSIFICATION_IN_PROGRESS && captureSingleFrame(frame)) { 
@@ -624,16 +645,18 @@ void show(const string& winname, InputArray mat) {
 
 void publishMovement(int state) {
     #ifdef ROS
-    // roboard_drivers::Motor motorMessage;
-    // motorMessage.left = 0.0f;
-    // motorMessage.right = 0.0f;
-    // rawMotorPublisher.publish(motorMessage);
+    if (USE_ROS) {
+        // roboard_drivers::Motor motorMessage;
+        // motorMessage.left = 0.0f;
+        // motorMessage.right = 0.0f;
+        // rawMotorPublisher.publish(motorMessage);
 
-    if (state == 4) log("Publish WALL\n");
-    if (state == 5) log("Publish STOPWALL\n");
-    MovementCommand mc;
-    mc.type = state;
-    movementPublisher.publish(mc);
+        if (state == 4) log("Publish WALL\n");
+        if (state == 5) log("Publish STOPWALL\n");
+        MovementCommand mc;
+        mc.type = state;
+        movementPublisher.publish(mc);
+    }
     #endif
 }
 
