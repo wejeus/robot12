a55aef082bf2f896271cbecc5f170dd64bc72e5c
Robot <robot@robot-vm.(none)>
2012-11-25 18:51:00 +0100

    added flag input to mother brain

--- amee/src/StrategyControl/MotherBrain.cpp
+++ amee/src/StrategyControl/MotherBrain.cpp
@@ -1,9 +1,12 @@
 #include "ros/ros.h"
 
+#include <stdio.h>
 #include <iostream>
 #include <std_msgs/Int32.h>
 #include "PhaseOneControl.h"
 #include "PhaseTwoControl.h"
+#include <unistd.h>
+
 
 using namespace std;
 
@@ -58,6 +61,9 @@ void executePhaseTwo(ros::NodeHandle nodeHandle) {
 	}
 }
 
+const int EXPLORE = 1;
+const int RESCUE = 2;
+int mMission;
 
 int main(int argc, char **argv) {
 	
@@ -66,10 +72,43 @@ int main(int argc, char **argv) {
 
 	// ros::Subscriber buttonOne = nodeHandle.subscribe("/StrategyControl/ButtonOne", 10, &buttonOneCallback);
 	// ros::Subscriber buttonTwo = nodeHandle.subscribe("/StrategyControl/ButtonTwo", 10, &buttonTwoCallback);
-	
+	int c;
+    while ((c = getopt (argc, argv, "re")) != -1) {
+        switch (c) {
+            case 'e': //explore (phase 1)
+            	mMission = EXPLORE;
+                break;
+            case 'r': // rescue (phase 2)
+            	mMission = RESCUE;
+				break;
+            case '?':
+                if (optopt == 's')
+                    printf("Option -%c requires an argument.\n", optopt);
+                else if (isprint (optopt))
+                    printf("Unknown option `-%c'.\n", optopt);
+                else
+                    printf("Unknown option character `\\x%x'.\n", optopt);
+                return 1;
+            default:
+                abort();
+        }
+    }
+
+    if (mMission == EXPLORE) {
+    	printf("Mission: Explore maze");
+    	executePhaseTwo(nodeHandle); 
+    	printf("Mission: Explore maze - Accomplished!");
+    } else if (mMission == RESCUE) {
+    	printf("Mission: Resce tags");
+    	executePhaseTwo(nodeHandle); 
+    	printf("Mission: Rescue tags - Accomplished!");
+    } else {
+        printf("Mission: Unknown");
+    }
 	// Testing code.
-	executePhaseTwo(nodeHandle);
-	cout << "done." << endl;
+
+	// executePhaseTwo(nodeHandle); 
+	// cout << "done." << endl;
 
 	// ros::Rate loop_rate(6);
 	// while(ros::ok()){
